9a2a31fae92085fab4f0eeb10a899ccebbcac2da
XavierJEsteve <xesteve@ufl.edu>
2021-04-16 11:52:11 -0400

    NOW we are getting the arg data in debugging function. have to remember that the arglist->data is a pointer to an arg. Have to use arglist->data->arg to get the arguments data.

--- src/util.cpp
+++ src/util.cpp
@@ -3,9 +3,9 @@
 
 void show_arguments(arglist_t * arglist){
     int count = 0;
-
+    //Note this will currently be "backwards"...arg[0] is the last one enterred 
     while(arglist != NULL){
-        std::cout << "Arglist [" << count <<  "] data: " << arglist->data  << std::endl;
+        std::cout << "Arglist [" << count <<  "] data: " << arglist->data->arg  << std::endl;
         arglist = arglist->next;
         count++;
     }
